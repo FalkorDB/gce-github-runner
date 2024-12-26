57741e184575d4845334963b62f9cf73bbd90e72
Dudi <16744955+dudizimber@users.noreply.github.com>
2024-12-26 18:02:06 +0200

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
-# Add Docker's official GPG key
-apt-get update
-apt-get install -y ca-certificates curl
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-chmod a+r /etc/apt/keyrings/docker.asc
+os_type=$(grep ^ID= /etc/os-release | cut -d= -f2)
 
-# Add the repository to Apt sources
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "${VERSION_CODENAME}") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+if [ "$os_type" == "centos" ] || [ "$os_type" == "rhel" ]; then
+ # Install dnf utils
+  dnf -y install dnf-plugins-core
+  dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 
-# Update the package list and install Docker
-apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  # Update the package list and install Docker
+  dnf -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+fi
+else
+  # Add Docker's official GPG key
+  apt-get update
+  apt-get install -y ca-certificates curl
+  install -m 0755 -d /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+  chmod a+r /etc/apt/keyrings/docker.asc
+  
+  # Add the repository to Apt sources
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "${VERSION_CODENAME}") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+  
+  # Update the package list and install Docker
+  apt-get update
+  apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+fi
